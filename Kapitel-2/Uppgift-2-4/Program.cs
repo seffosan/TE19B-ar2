e6d70ecbe8cc62978e68079d397c4f73687a7258
Sef Abdu <sef.abdu@learnet.se>
2020-09-16 15:06:43 +0200

    övat på variabler

---
+++ Kapitel-2/Uppgift-2-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args);
+        {
+            int tal2 = int.Parse(Console.ReadLine());
+
+        Console.Write("ange heltal")
+
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
---
+++ Kapitel-2/Uppgift-2-4/Uppgift-2-5/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Skapa ett program som frågar användaren vilket år det är, därefter ska programmet säga hur många år det är kvar till år 2100.
+ */
+using System;
+
+namespace Uppgift_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läsa in ett årtal
+            Console.Write("ange ett årtal: ");
+            int årtal = int.Parse(Console.ReadLine());
+
+            // Räkna ut antal år til 2100
+            int antalÅr = 2100 - årtal;
+
+            // skriv ut resultatet
+        }
+        Console.writline("antal år kvar till 2100 är " + antalÅr);
+    }
+}
