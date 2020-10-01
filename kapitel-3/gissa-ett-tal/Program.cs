4ce75ae1c35a69664fb4c2597ebfa33a01b00a6a
Sef Abdu <sef.abdu@learnet.se>
2020-10-01 11:06:16 +0200

    tränat på if och loop

---
+++ kapitel-3/gissa-ett-tal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace gissa_ett_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //slumpa fram ett tal
+            Random slump = new Random();
+            int slumptal = slump.Next(1, 7);
+            //läs in en gissning
+            Console.WriteLine("gissa ett tal (1-6)? ");
+            int gissning = int.Parse(Console.ReadLine());
+
+            //kolla om gissningen är korrekt
+            if (gissning == slumptal)
+            {
+                Console.WriteLine("rätt svar");
+            }
+            else
+            {
+                Console.WriteLine("fel svar!");
+            }
+        }
+
+    }
+}
