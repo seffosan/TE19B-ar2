866c66ee6c7378ff2352ff87b9a85a9b5a16d4e6
Sef Abdu <sef.abdu@learnet.se>
2020-12-09 14:42:02 +0100

    tränat på array medan du var borta i utbildningen

---
+++ Kapitel-5/Uppgift-5-12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift_5_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = {"vad är meningen med livet?", "LORD GIVE ME A SIGN!", "inget tecken, antar att allt bara är en bluff."};
+
+            foreach (string mening in meningar)
+            {
+                Console.WriteLine(mening);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Kapitel-5/Uppgift-5-13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift_5_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
