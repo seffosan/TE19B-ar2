26f91882cff0f116838eabd939995070209d4b15
Seffosan <sef.abdu@elev.ga.ntig.se>
2020-12-18 12:29:12 +0100

    Font family singular

---
+++ Kapitel-5/Labyrint/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Labyrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa en labyrint
+            int[,] labyrint = {
+                {1, 1, 1, 1, 1, 1, 1},  // 0
+                {1, 0, 0, 0, 0, 0, 1},  // 1
+                {1, 0, 1, 1, 1, 0, 1},  // 2
+                {1, 0, 1, 1, 0, 0, 1},  // 3
+                {1, 0, 1, 1, 0, 1, 1},  // 4
+                {1, 0, 0, 0, 0, 0, 1},  // 5
+                {1, 0, 1, 1, 1, 1, 1}   // 6
+            };
+
+            // Slumpa fram in en figur i labyrinten
+            Random slumpMotor = new Random();
+            while (true)
+            {
+                // Slumpa fram koordinater
+                int x = slumpMotor.Next(0, 7);  // 0-6
+                int y = slumpMotor.Next(0, 7);  // 0-6
+
+                // Hamnar vi i en gång
+                if (labyrint[y, x] == 0)
+                {
+                    labyrint[y, x] = 2;
+                    break;
+                }
+            }
+
+            // För att skriva ut Entities
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            // Skriva ut labyrinten
+            // Loopa igenom rad för rad, dvs i y-led
+            for (int y = 0; y < 7; y++)
+            {
+                // Loopa igenom kolumnvis, dvs x-led
+                for (int x = 0; x < 7; x++)
+                {
+                    switch (labyrint[y, x])
+                    {
+                        case 1: // En vägg
+                        Console.Write('\u2B1C');
+                        break;
+
+                        case 2: // En figur
+                        Console.Write("⌚");
+                        break;
+
+                        default: // En gång
+                        Console.Write('\u2B1B');
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Kapitel-5/Uppgift-5-13/Program.cs
+++ Kapitel-5/Uppgift-5-13/Program.cs
@@ -6,7 +6,34 @@ namespace Uppgift_5_13
     {
         static void Main(string[] args)
         {
-            
+            int[,] labyrint = {
+                {1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 1},
+                {1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 0},
+                {1, 0, 1, 1, 1},
+            };
+
+            //för att skriva ut UTF-tecken
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            //loopa igenom raderna 0-4
+            for (int y = 0; y < 5; y++)
+            {
+                //loopa igenom cellerna o-4
+                for (int x = 0; x < 5; x++)
+                {
+                    //om i skriv ut vit fyrkant, annars svart fyrkant
+                    if (labyrint[y, x] == 1)
+                    {
+                        Console.Write('\u2B1C');
+                    } else
+                    {
+                        Console.Write('\u2B1B');
+                    }
+                }
+                Console.WriteLine(); 
+            }
         }
     }
 }
